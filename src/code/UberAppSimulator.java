bfed878153ce62f0686fd9566d4290103a5432d2
Jessie Smith <smithygirl@gmail.com>
2017-10-09 12:38:58 -0700

    update simulator to test edge cases

--- src/code/UberAppSimulator.java
+++ src/code/UberAppSimulator.java
@@ -2,12 +2,20 @@ package code;
 
 public class UberAppSimulator {
 	public static void main(String [] args) {
-		Driver testDriver = new Driver();
-		Passenger testPassenger = new Passenger();
-		Driver[] drivers = new Driver[1];
-		Passenger[] passengers = new Passenger[1];
-		drivers[0] = testDriver;
-		passengers[0] = testPassenger;
+		Driver testDriver1 = new Driver();
+		Driver testDriver2 = new Driver();
+		Driver testDriver3 = new Driver();
+		Passenger testPassenger1 = new Passenger();
+		Passenger testPassenger2 = new Passenger();
+		Passenger testPassenger3 = new Passenger();
+		Driver[] drivers = new Driver[3];
+		Passenger[] passengers = new Passenger[3];
+		drivers[0] = testDriver1;
+		drivers[1] = testDriver2;
+		drivers[2] = testDriver3;
+		passengers[0] = testPassenger1;
+		passengers[1] = testPassenger2;
+		passengers[2] = testPassenger3;
 		MapGrid testMap = new MapGrid(5, drivers, passengers);
 		testMap.PrintMap();
 	}
